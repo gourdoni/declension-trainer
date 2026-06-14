3c6d641dc7ece72793ef4ddf8bdddad7a8f19d1b
Harry Porter <h.porter23@icloud.com>
2026-06-14 15:00:20 +1000

    Adds basic HTTP server skeleton

--- src/main/java/io/github/gourdoni/declension/App.java
+++ src/main/java/io/github/gourdoni/declension/App.java
@@ -1,8 +1,23 @@
 package io.github.gourdoni.declension;
 
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.charset.StandardCharsets;
+
 public class App {
-    public static void main(String[] args) {
-        String greeting = "Salvete!";
-        System.out.println(greeting);
+    public static void main(String[] args) throws java.io.IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            byte[] body = "Salvete! Valete!".getBytes(StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().set("Content-Type", "text/plain; charset=utf-8");
+            exchange.sendResponseHeaders(200, body.length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(body);
+            }
+        });
+        server.start();
+        System.out.println("Server running at http://localhost:8080!");
     }
 }
