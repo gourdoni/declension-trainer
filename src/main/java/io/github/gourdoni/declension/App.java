67681e616997348a384f5407ed5f2f33b116c23c
Harry Porter <h.porter23@icloud.com>
2026-06-14 13:25:06 +1000

    Adds minimal entry point class

---
+++ src/main/java/io/github/gourdoni/declension/App.java
@@ -0,0 +1,8 @@
+package io.github.gourdoni.declension;
+
+public class App {
+    public static void main(String[] args) {
+        String greeting = "Salvete!";
+        System.out.println(greeting);
+    }
+}
